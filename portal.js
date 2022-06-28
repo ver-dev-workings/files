ad0bfb121d06206f6abf2f0bdad14c6e49de8243
Bilal Ahmed Abbas <84194687+bilibilal1@users.noreply.github.com>
2022-06-28 15:15:58 +0100

    Update portal.js

--- portal.js
+++ portal.js
@@ -191,7 +191,8 @@ var _app = {
 	    _app.informUser(1, _app.alerts.logout); 
     },
     hardLogOut: function() { 
-	 document.getElementById('nav_logout').submit();    
+	 window.location.href = 'https://lobe-dev-web01.squiz.cloud/_webservices/esi/logout-popup' +'?redirectUri=' + window.location.href;   
+	/* document.getElementById('nav_logout').submit(); */   
     },
     setUser: function(s) {
         _app.user.uName = s;
