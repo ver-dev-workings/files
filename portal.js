1a1fd77e1717af7c569dc019ee0eed8e3bd45004
Neetu Dave <web.team@enfield.gov.uk>
2022-09-21 10:09:07 +0100

    Update portal.js

--- portal.js
+++ portal.js
@@ -349,20 +349,9 @@ _app.addFavicons();
 $(document).ready(function() {
     _app.init();
 	_app.customMember();
-
-	var s = document.createElement("script");
-	s.type = "text/javascript";
-	s.src = "https://websurveys2.govmetric.com/js/client/gm_intro.js?v=202011112031";
-	$("head").append(s);
-	
-	var l = document.createElement("link");
-	l.rel = "stylesheet";
-	l.type = "text/css";
-	l.href = "https://websurveys2.govmetric.com//css/modal2/modal.css?v=202011112031";
-	$("head").append(l);
 });
 
-var addGovmetric = '<div class="gm_sidebar gm_sidebar_rh" style="z-index:0"><a href="https://websurveys2.govmetric.com/theme/gm/2634" class="gm_sidebar_anchor" role="button" rel="nofollow"><img src="https://websurveys2.govmetric.com/imgs/triggers/rh.svg" alt="Feedback button"></a></div><link rel="stylesheet" type="text/css" href="https://websurveys2.govmetric.com/css/client/gm_sidebar_rh.css"><script type="text/javascript" src="https://websurveys2.govmetric.com/js/client/gm_sidebar.js"></script><script type="text/javascript" src="https://hitcounter.govmetric.com/2635"></script>'; // string of HTML here
+var addGovmetric = '<!-- GovMetric Web Code Snippet starts here --><div class="gm_sidebar gm_sidebar_rh" style="z-index:0"><a href="https://websurveys2.govmetric.com/theme/gm/2634" class="gm_sidebar_anchor" role="button" rel="nofollow"><img src="https://websurveys2.govmetric.com/imgs/triggers/rh.svg" alt="Feedback button"></a></div><link rel="stylesheet" type="text/css" href="https://websurveys2.govmetric.com/css/client/gm_sidebar_rh.css"><script type="text/javascript" src="https://websurveys2.govmetric.com/js/client/gm_sidebar.js"></script><script type="text/javascript" src="https://hitcounter.govmetric.com/2635"></script><!-- GovMetric Web Code Snippet ends here -->'; // string of HTML here
 ready(function() {
 	document.body.insertAdjacentHTML('beforeend', addGovmetric);
 })
