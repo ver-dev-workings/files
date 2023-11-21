85f26e65b5041a0858a245f2951e5f028743d52e
Bilal Ahmed Abbas <84194687+bilibilal1@users.noreply.github.com>
2023-11-21 16:57:18 +0000

    Update portal.js

--- portal.js
+++ portal.js
@@ -371,10 +371,10 @@ _app.addFavicons();
 $(document).ready(function() {
     _app.init();
 	_app.customMember();
-	_app.CSRFCookie();
+	//_app.CSRFCookie();
 	//var csrfToken = $('meta[name="_csrf_token"]').attr('content');
 	//console.log(csrfToken);
-	console.log("16");
+	console.log("17");
 });
 
 /*var addGTM = '<!-- Google Tag Manager (noscript) --><noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N36QQRP" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript><!-- End Google Tag Manager (noscript) -->';
