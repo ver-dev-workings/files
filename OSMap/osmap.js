bfb5dda8acb5df60ab20f48a3e0eb4f45340a038
Neetu Dave <web.team@enfield.gov.uk>
2024-03-12 15:58:29 +0000

    Update osmap.js

--- OSMap/osmap.js
+++ OSMap/osmap.js
@@ -28,7 +28,10 @@ function do_KDF_pageChange_OSMap(event, kdf, currentpageid, targetpageid) {
 function initialiseOSMap(mapHolder) {
   if (!$(mapHolder).is(":visible") || $(mapHolder).attr("data-mapready"))
     return;
+  
   $(mapHolder).attr("data-mapready", true);
+  if($(window).width() <= 1024)
+        $("#dform_widget_search_ps_property_search_map").after('<button type="button" class="getMyLocation" id="getMyLocation0"><svg width="36px" height="36px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M19 12C19 15.866 15.866 19 12 19M19 12C19 8.13401 15.866 5 12 5M19 12H21M12 19C8.13401 19 5 15.866 5 12M12 19V21M5 12C5 8.13401 8.13401 5 12 5M5 12H3M12 5V3M15 12C15 13.6569 13.6569 15 12 15C10.3431 15 9 13.6569 9 12C9 10.3431 10.3431 9 12 9C13.6569 9 15 10.3431 15 12Z" stroke="#C41508" stroke-width="1.08" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>Use my location</button>');
   map = L.map("map").setView([51.653046, -0.08958], 12);
   var baseLayer = L.tileLayer(
     serviceUrl + "/Outdoor_3857/{z}/{x}/{y}.png?key=" + apiKey,
