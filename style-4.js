966815ceff22235c8ac6ce1b94f60dea2e475884
Bilal Ahmed Abbas <84194687+bilibilal1@users.noreply.github.com>
2024-09-10 15:56:57 +0100

    Updated GTM

--- style-4.js
+++ style-4.js
@@ -4,9 +4,19 @@
 =====================================
 **/
 
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], 
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})
-(window,document,'script','dataLayer','GTM-N36QQRP');
+(function(w,d,s,l,i){
+    w[l]=w[l]||[];
+    w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+    var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),
+        dl=l!='dataLayer'?'&l='+l:'';
+    j.async=true;
+    j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl+
+          '&gtm_auth=ULiWAyI-DCcawxQWr1dLjA' +
+          '&gtm_preview=env-83' +
+          '&gtm_cookies_win=x';
+    f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N36QQRP');
 
 /**
 =====================================
